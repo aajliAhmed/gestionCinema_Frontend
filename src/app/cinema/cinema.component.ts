0136cbf5a6e08fbc523aa8dd6a5dd1ec1c23921b
aajli ahmed <ahmed.conceptor@gmail.com>
2023-08-21 16:51:56 +0100

    création de component cinema pour afficher les cinemas et ces detailles et création de dossiers service

--- src/app/cinema/cinema.component.ts
+++ src/app/cinema/cinema.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { CinemaService } from '../services/cinema.service';
 
 @Component({
   selector: 'app-cinema',
   templateUrl: './cinema.component.html',
   styleUrls: ['./cinema.component.css']
 })
-export class CinemaComponent {
+export class CinemaComponent implements OnInit {
 
+  public villes: any;
+  public cinemas: any;
+  public currentVille: any;
+  public currentCinema: any;
+  public salles: any;
+
+  constructor(private cinemaServise: CinemaService) { }
+  ngOnInit() {
+    this.cinemaServise.getVilles().subscribe(data => {
+      this.villes = data;
+    }, err => {
+      console.log(err);
+    })
+
+  }
+  onGetCinemas(v: any) {
+    this.currentCinema = v;
+    this.cinemaServise.getCinemas(v).subscribe(data => {
+      this.cinemas = data;
+    }, err => {
+      console.log(err);
+    })
+  }
+  onGetSalles(c:any){
+    this.currentCinema=c;
+    this.cinemaServise.getSalles(c).subscribe(data => {
+      this.salles = data;
+    }, err => {
+      console.log(err);
+    })
+  }
 }
---
+++ src/app/services/cinema.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CinemaService {
+  public host:string="http://localhost:8080";
+  constructor(private http:HttpClient) {}
+  public getVilles(){
+      return this.http.get(this.host+"/villes");
+   }
+   public getCinemas(v:any){
+     return  this.http.get(v._links.cinemas.href);
+   }
+   public getSalles(c:any){
+    return  this.http.get(c._links.salles.href);
+   }
+}
